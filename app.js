ecf3d49e8bb915fb0f8257ed434893f30c9962e9
Simar Chadha <simarchadha1@gmail.com>
2023-12-04 18:44:29 -0500

    A6 routes

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import UserRoutes from "./users/routes.js";
+import mongoose from "mongoose";
+import session from "express-session";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(
+  session(sessionOptions)
+);
 
 app.use(express.json());
+
+UserRoutes(app);
 
 ModuleRoutes(app);
 CourseRoutes(app);
